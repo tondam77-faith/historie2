51ce50e2af67bb4862fc77c16962450545e0e138
tondam77-faith <tondam77@gmail.com>
2026-01-17 17:30:57 +0100

    Verze 36

--- sw.js
+++ sw.js
@@ -1,13 +1,31 @@
-const CACHE_NAME = 'historie-image-v01';
+const CACHE_NAME = 'historie-srs-v36'; // ZMĚNA NA v36
 const ASSETS = [
-  'index.html',
-  'manifest.json'
+  './',
+  './index.html',
+  './manifest.json',
+  './history.png' // OPRAVENO: sjednoceno s názvem v manifestu
 ];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim();
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
